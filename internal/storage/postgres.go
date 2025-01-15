f7b710bbdb2b3b7440e3b20831fb854928e8c831
DenHax <whatxpw@gmail.com>
2025-01-15 23:49:26 +0300

    Move storage init in package

---
+++ internal/storage/postgres.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	EmployeeTable = "Employee"
+)
+
+type Storage struct {
+	DB *sqlx.DB
+}
+
+func New(connUrl string) (*Storage, error) {
+	const op = "storage.postgres.New"
+
+	db, err := sqlx.Open("postgres", connUrl)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	fmt.Println("Connection!")
+
+	return &Storage{DB: db}, nil
+}
+
+func (s *Storage) Close() error {
+	fmt.Println("Close storage")
+	return s.DB.Close()
+}
