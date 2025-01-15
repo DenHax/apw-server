c69b3439aba3bda17f3f0e5effe7ad03ab3a1e46
DenHax <whatxpw@gmail.com>
2025-01-15 23:49:02 +0300

    Init and start web server

---
+++ internal/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"apw/internal/config"
+	"context"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func New(scfg config.ServerConfig, handler http.Handler) *Server {
+	srv := new(Server)
+	srv.httpServer = &http.Server{
+		Addr:           scfg.Address,
+		Handler:        handler,
+		ReadTimeout:    scfg.ReadTimeout,
+		WriteTimeout:   scfg.WriteTimeout,
+		MaxHeaderBytes: 1 << 20, // 1 MB
+	}
+	return srv
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
