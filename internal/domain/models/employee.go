1e2c7885e1043c50e78326f11e3203b502cb76e1
DenHax <whatxpw@gmail.com>
2025-01-19 21:55:46 +0300

    Refactoring employee and upload models, add new models

--- internal/domain/models/employee.go
+++ internal/domain/models/employee.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type Employee struct {
 	EmpoyeeId   *int    `json:"employee_id" db:"employee_id"`
 	Firstname   string  `json:"firstname" db:"employee_firstname"`
@@ -8,4 +10,19 @@ type Employee struct {
 	SubsystemId int     `json:"subsystem_id" db:"subsystem_id"`
 	Title       string  `db:"employee_title" json:"title"`
 	Phone       string  `db:"employee_phone" json:"phone"`
+}
+
+type UpdateEmployee struct {
+	Firstname   *string `json:"firstname" db:"employee_firstname"`
+	Surname     *string `json:"surname" db:"employee_surname"`
+	Lastname    *string `json:"lastname" db:"employee_lastname"`
+	SubsystemId *int    `json:"subsystem_id" db:"subsystem_id"`
+	Phone       *string `json:"phone" db:"employee_phone"`
+}
+
+func (i UpdateEmployee) Validate() error {
+	if i.Firstname == nil && i.Surname == nil && i.Lastname == nil && i.SubsystemId == nil && i.Phone == nil {
+		return errors.New("update structure has no values")
+	}
+	return nil
 }
--- internal/domain/models/upload.go
+++ internal/domain/models/upload.go
@@ -4,9 +4,16 @@ import "time"
 
 type Upload struct {
 	LoadDate         time.Time `db:"load_date" json:"load_date"`
-	EmployeeId       int       `db:"employee_id" json:"employee_id"`
+	EmployeeId       *int      `db:"employee_id" json:"employee_id"`
 	EmployeeFullName string    `json:"employee_full_name" db:"employee_full_name"`
 	SubsystemId      *int      `db:"subsystem_id" json:"subsystem_id"`
-	SubsystemName    *string   `json:"subsystem_name" db:"subsystem_name"`
 	FuelRoadNumber   int       `db:"fuel_road_number" json:"fuel_road_number"`
+}
+
+type Report struct {
+	EmployeeFullName string    `json:"employee_full_name" db:"employee_full_name"`
+	SubsystemId      int       `json:"subsystem_id" db:"subsystem_id"`
+	LoadCount        int       `json:"load_count" db:"load_count"`
+	FirstLoadDate    time.Time `json:"first_load_date" db:"first_load_date"`
+	LastLoadDate     time.Time `json:"last_load_date" db:"last_load_date"`
 }
